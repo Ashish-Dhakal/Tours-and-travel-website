0c5b51ccd3a45832e23483760e164330b355ffca
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-02-19 21:00:50 +0545

    footer added

--- home.php
+++ home.php
@@ -34,38 +34,84 @@
         </nav>
 
         <div id="menu-btn" class="fas fa-bars">
+        </div>
 
+    </section>
+
+    <!-- header section end here -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer section start here  -->
+
+    <section class="footer">
+        <!-- <img src="img/footer-img1.jpg" alt=""> -->
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"> </i> Home </a>
+                <a href="about.php"> <i class="fas fa-angle-right"> </i> About</a>
+                <a href="package.php"> <i class="fas fa-angle-right"> </i> Package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"> </i> Book</a>
+            </div>
+
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"> <i class="fas fa-angle-right"> </i> Ask question </a>
+                <a href="#"> <i class="fas fa-angle-right"> </i> faq</a>
+                <a href="#"> <i class="fas fa-angle-right"> </i> about us </a>
+                <a href="#"> <i class="fas fa-angle-right"> </i> privacy policy </a>
+                <a href="#"> <i class="fas fa-angle-right"> </i> terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>Contact Info</h3>
+                <a href="#"> <i class="fas fa-phone"> </i> +977-9863379537 </a>
+                <a href="#"> <i class="fas fa-envelope"> </i>ashishdhakal433@gmail.com</a>
+                <a href="#"> <i class="fas fa-location-dot"> </i> Pokhara Chhorepatan-17 </a>
+            </div>
+
+
+
+            <div class="box">
+                <h3> Follow us</h3>
+                <a href="#"> <i class="fab fa-facebook"> </i> facebook</a>
+                <a href="#"> <i class="fab fa-instagram"> </i>instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+            </div>
 
         </div>
+        <div class="credit"> Created by <span>Ashish Dhakal</span> | All rights reserved!</div>
 
     </section>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <!-- footer section end here -->
 
     <!-- swiper script link -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
