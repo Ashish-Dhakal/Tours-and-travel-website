753b1c75d661c735321e39a4ad9a201887fcb817
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-02-26 21:37:44 +0545

    script to load next three box on clicking a load more btn

--- script.js
+++ script.js
@@ -43,3 +43,21 @@ var swiper = new Swiper(".reviews-slider", {
   },
 });
 
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+  for (var i = currentItem; i < currentItem + 3; i++)
+  {
+    boxes[i].style.display = 'inline-block'; 
+
+  };
+  currentItem += 3;
+  if (currentItem >= boxes.length)
+  {
+    loadMoreBtn.style.display = "none";
+    }
+}
+
+
