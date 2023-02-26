102631c053b299e2969e02f60b9604cf51ae3981
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-02-26 23:04:42 +0545

    book content added

--- book.php
+++ book.php
@@ -49,8 +49,64 @@
 
     <div class="heading">
         <h1>Book now</h1>
-
     </div>
+
+    <!-- booking section starts -->
+
+    <section class="booking">
+
+        <h1 class="heading-title"> book your trip!</h1>
+
+        <form action="book_form.php" method="post" class="book-form">
+            <div class="flex">
+                <div class="inputBox">
+                    <span> name :</span>
+                    <input type="text" placeholder="enter your name" name="name">
+                </div>
+
+                <div class="inputBox">
+                    <span> email :</span>
+                    <input type="email" placeholder="enter your email" name="email">
+                </div>
+
+                <div class="inputBox">
+                    <span> phone :</span>
+                    <input type="number" placeholder="enter your phone number" name="phone">
+                </div>
+
+                <div class="inputBox">
+                    <span> address :</span>
+                    <input type="text" placeholder="enter your address" name="address">
+                </div>
+
+                <div class="inputBox">
+                    <span> where to :</span>
+                    <input type="text" placeholder="place you want to visite" name="location">
+                </div>
+
+                <div class="inputBox">
+                    <span> how many:</span>
+                    <input type="text" placeholder="how many guest" name="guests">
+                </div>
+
+                <div class="inputBox">
+                    <span> arrival :</span>
+                    <input type="date" name="arrival">
+                </div>
+
+                <div class="inputBox">
+                    <span> leaving :</span>
+                    <input type="date" name="leaving">
+                </div>
+            </div>
+
+            <input type="submit" name="send" value="submit" class="btn">
+
+        </form>
+
+    </section>
+
+    <!-- booking section ends -->
 
 
 
